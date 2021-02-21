5e1676f3adaf17041c22a7c593dbfa6e164977d4
Filip Peterek <filip.peterek@firma.seznam.cz>
2021-02-21 15:51:45 +0100

    Initial commit

---
+++ SourceCodes/ArraySortingAlgorithms.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Rewritten so as to avoid people accusing me of distributing their " <<
+        << "publicly available source code." << std::endl;
+    std::cout << "This code won't make it the final version, anyway." << std::endl;
+}
+
---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pdflatex main
+biber main
+pdflatex main
+pdflatex main
+
