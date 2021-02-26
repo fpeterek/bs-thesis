9932ed5954f09ae254b7ab0c6a2e2cabe0697736
Filip Peterek <fpeterek@seznam.cz>
2021-02-26 20:56:23 +0100

    Fixed space characters

--- SourceCodes/ArraySortingAlgorithms.cpp
+++ SourceCodes/ArraySortingAlgorithms.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[]) {
     std::cout << "Rewritten so as to avoid people accusing me of distributing their " <<
         << "publicly available source code." << std::endl;
-    std::cout << "This code won't make it the final version, anyway." << std::endl;
+    std::cout << "This code won't make it the final version, anyway." << std::endl;
 }
 
