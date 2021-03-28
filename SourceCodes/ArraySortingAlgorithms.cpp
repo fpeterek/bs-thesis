038b8f3213602728bb2d4533da1ae4666ffcc1ad
Filip Peterek <fpeterek@seznam.cz>
2021-03-28 13:34:08 +0200

    Migration to new template

--- SourceCodes/ArraySortingAlgorithms.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    std::cout << "Rewritten so as to avoid people accusing me of distributing their " <<
-        << "publicly available source code." << std::endl;
-    std::cout << "This code won't make it the final version, anyway." << std::endl;
-}
-
